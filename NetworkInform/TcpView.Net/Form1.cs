020e9d5055460019f1912552aa4faef3a4cce52c
Pasha Ivasiuta <pash.ok13@yandex.ua>
2019-03-17 20:16:22 +0200

    add win forms project

---
+++ NetworkInform/TcpView.Net/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Windows.Forms;
+using MTorrent;
+using System.ComponentModel;
+
+namespace TcpView.Net
+{
+    public partial class Form1 : Form
+    {
+        private List<Connection> _allConnections = new List<Connection>();
+        private int selectedProcessId = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            UpdateInfo();
+				//dataGridView1.DataSource = _allConnections;
+
+          // ExecutionContext.
+        }
+
+        private void CheckBox3CheckedChanged(object sender, EventArgs e)
+        {
+            WriteToGrid(selectedProcessId);
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+            UpdateInfo();
+//             WriteToGrid(selectedProcessId);
+            UpdateProcesses();
+        }
+
+        private void UpdateInfo()
+        {
+            _allConnections.Clear();
+
+            _allConnections.AddRange(NetworkInformation.GetTcpV4Connections());
+            _allConnections.AddRange(NetworkInformation.GetTcpV6Connections());
+            _allConnections.AddRange(NetworkInformation.GetUdpV4Connections());
+            _allConnections.AddRange(NetworkInformation.GetUdpV6Connections());
+        }
+
+        private void UpdateProcesses()
+        {
+            Connection.UpdateProcessList();
+
+			dataGridView1.Rows.Clear();
+
+			for (int index = 0; index < Connection.Processes.Length; index++)
+            {
+                Process process = Connection.Processes[index];
+
+                var item = new ListViewItem();
+                item.Text = process.ProcessName;
+                item.ImageKey = process.Id.ToString();
+                item.Tag = process.Id;
+
+				WriteToGrid(process.Id);
+			}
+
+		}
+
+        private void ListView1SelectedIndexChanged(object sender, EventArgs e)
+        {
+			var list = (ListView)sender;
+			if (list.SelectedItems.Count > 0)
+			{
+				selectedProcessId = (int)(list.SelectedItems[0].Tag);
+				WriteToGrid(selectedProcessId);
+			}
+		}
+
+        private void WriteToGrid(int procId)
+        {
+            int endPoints = 0;
+
+            int tcpv4 = 0, udpv4 = 0, tcpv6 = 0, udpv6 = 0;
+            int listening = 0;
+
+            for (int index = 0; index < _allConnections.Count; index++)
+            {
+                Connection info = _allConnections[index];
+
+                bool ok = false;
+
+                switch (info.ConnectType)
+                {
+                    case ConnectType.Tcp:
+                        if (checkBoxTcp.Checked && checkBoxV4.Checked) ok = true;
+                        ++tcpv4;
+                        break;
+                    case ConnectType.Udp:
+                        if (checkBoxUdp.Checked && checkBoxV4.Checked) ok = true;
+                        ++udpv4;
+                        break;
+                    case ConnectType.TcPv6:
+                        if (checkBoxTcp.Checked && checkBoxV6.Checked) ok = true;
+                        ++tcpv6;
+                        break;
+                    case ConnectType.UdPv6:
+                        if (checkBoxUdp.Checked && checkBoxV6.Checked) ok = true;
+                        ++udpv6;
+                        break;
+                }
+
+                if (info.State == TcpState.Listen)
+                    ++listening;
+
+                if (info.RemoteEndPoint.Address.Equals(IPAddress.IPv6Any)
+                    || info.RemoteEndPoint.Address.Equals(IPAddress.Any))
+                {
+                    if (!CBoxunconEndPoints.Checked)
+                        continue;
+                }
+                else
+                    ++endPoints;
+
+                if (procId >= 0 && procId != info.OwningPid)
+                    ok = false;
+
+                if (ok)
+                {
+                    dataGridView1.Rows.Add(info.OwningProcess, info.ConnectType, info.LocalEndPoint,
+                                           info.RemoteEndPoint, info.State /*,info.DnsName*/);
+                }
+            }
+
+
+            toolStripStatusLabel1.Text = string.Format("TCPv4 {0}", tcpv4);
+            toolStripStatusLabel2.Text = string.Format("TCPv6 {0}", tcpv6);
+
+            toolStripStatusLabel3.Text = string.Format("UDPv4 {0}", udpv4);
+            toolStripStatusLabel4.Text = string.Format("UDPv6 {0}", udpv6);
+
+            toolStripStatusLabel5.Text = string.Format("Listen {0}", listening);
+            toolStripStatusLabel6.Text = string.Format("EndPoints {0}", endPoints);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //timer1.Interval = 1000;
+           // UpdateProcesses();
+          //  Connection.UpdateProcessList();
+        //    WriteToGrid(selectedProcessId);
+//             _allConnections = NetworkInformation.GetProcessTcpActivity(selectedProcessId);
+//             dataGridView1.DataSource = _allConnections;
+        }
+    }
+}
---
+++ NetworkInform/TcpView.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+
+[assembly: AssemblyTitle("TcpView.Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TcpView.Net")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+
+[assembly: Guid("5a1abc2b-c0b5-43e2-bfda-8ee719d7b1c5")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
